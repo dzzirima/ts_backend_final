50362fd4abbe2f67551a90213827b4feca737ee8
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-10-21 15:51:34 +0200

    getting started with swagger

---
+++ src/utils/swagger.ts
@@ -0,0 +1,5 @@
+import { Express, Response ,Request } from "express";
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import {version } from "../../package.json"
+import log from "./logger"
